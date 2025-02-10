e6057dca6437f8e15d2a65de1c42dc31121f7bb5
Petar <iosdev.petar@gmail.com>
2025-02-10 13:07:25 +0100

    Initial commit. Created project. Imported resources

---
+++ KickOff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KickOff
+//
+//  Created by Petar  on 10.2.25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KickOff/KickOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickOffApp.swift
+//  KickOff
+//
+//  Created by Petar  on 10.2.25..
+//
+
+import SwiftUI
+
+@main
+struct KickOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
